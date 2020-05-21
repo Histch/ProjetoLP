9f03abee51510ab3eedf5b7910786b813a1d712b
Leonardo Carvalho <170100058@esg.ipsantarem.pt>
2020-05-21 19:33:29 +0100

    Criação do meu menu de navegação

--- Programa_Projeto/main.java
+++ Programa_Projeto/main.java
@@ -1,1 +1,20 @@
-asdgasdjhasdasd
+
+
+public class Main{
+
+    public static void main (String[] args) {
+
+        System.out.println(" ------------ MENU ------------");
+
+        System.out.println("");
+
+        System.out.println(" 1 - Saber a nossa localização ");
+        System.out.println(" 2 - Árvore Binária visual ");
+        System.out.println(" 3 - Quantidade de Países ou Cidades (Açores ou Madeira) existentes ");
+        System.out.println(" 4 - Procurar Países ou Cidades (Açores, Madeira) ");
+        System.out.println(" 5 - Sair ");
+        
+        System.out.println("");
+        
+    }
+}
